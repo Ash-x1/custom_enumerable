71037d86c5a87f2233e6f00cca14b015bd78ed79
Ash-x1 <ash91p@proton.me>
2026-01-20 17:44:24 +0100

    Add solutions for custom enumerable exercise

---
+++ lib/my_enumerables.rb
@@ -0,0 +1,19 @@
+module Enumerable
+  # Your code goes here
+  def my_each
+    self.each do |array|
+      p yield(array)
+    end
+  end
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+  end
+end
